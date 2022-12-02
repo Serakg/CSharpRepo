fe3082da8f860b04fffcbf0d295024a8f989b4bb
Serák Gábor <92935059+Serakg@users.noreply.github.com>
2022-12-02 13:30:06 +0100

    Delete WebHello directory

--- WebHello/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace WebHello.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
