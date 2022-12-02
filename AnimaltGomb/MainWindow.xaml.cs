8bc6ee9e443d973d62b84f2fcd16f443a4279f9c
Gabor Serak <gabor.serak@assaabloy.com>
2022-12-02 10:44:57 +0100

    AnimaltGomb Unit nélkül

---
+++ AnimaltGomb/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace AnimaltGomb
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void mainBtn_Click(object sender, RoutedEventArgs e)
+        {
+            int angel = 15;
+            mainBtn.LayoutTransform= new RotateTransform(angel);
+        }
+    }
+}
