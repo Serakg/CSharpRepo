7b89197a8aa9f38cbaec80eaf8f4485661dfd2c3
Gabor Serak <gabor.serak@assaabloy.com>
2022-12-05 11:27:33 +0100

    WebHello Unit nélkül

---
+++ WebHello/Controllers/LoginController.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using WebHello.Models;
+
+namespace WebHello.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            Login login = new Login();
+            login.Nev = "Serák Gábor";
+            login.LatogatasIdeje = DateTime.Now;
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Sikeres(Login log)
+        {
+            TextWriter? txt = null;
+            if (ModelState.IsValid)
+            {
+                string JsongValue = JsonConvert.SerializeObject(log);
+                string logfile = "C:\\Users\\gabser\\Documents\\WebhelloLog\\Login log" + ".txt";
+                if(System.IO.File.Exists(logfile)) 
+                {
+                    txt = new StreamWriter(logfile, append: true);
+                }
+                else
+                {
+                    txt = new StreamWriter(logfile, append: true);
+                }
+
+                txt.WriteLine(JsongValue);
+                txt.Close();
+            }
+            return View(log);
+        }
+    }
+}
---
+++ WebHello/Models/Login.cs
@@ -0,0 +1,8 @@
+﻿namespace WebHello.Models
+{
+    public class Login
+    {
+        public string Nev { get; set; }
+        public DateTime LatogatasIdeje { get; set; }
+    }
+}
